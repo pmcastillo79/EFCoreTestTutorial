2e6c96ac887bd6be7a46fc6e37e34b4f25c0fd9b
Pablo Castillo <63909791+pcastillocu@users.noreply.github.com>
2020-10-27 22:53:25 -0400

    V1 for Microsoft EFCore Team
    this gave me error with Add-Migration command

---
+++ CrudEntityFramework/CrudEntityFramework/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CrudEntityFramework.Models;
+using CrudEntityFramework.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudEntityFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Usuario.ToListAsync());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
